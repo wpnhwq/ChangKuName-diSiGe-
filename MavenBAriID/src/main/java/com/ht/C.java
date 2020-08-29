5ab971058b59243f4b92c117297fc55148a9baed
painan <112233@qq.com>
2020-08-30 00:27:22 +0800

    提交MavenBAriID项目

---
+++ MavenBAriID/src/main/java/com/ht/C.java
@@ -0,0 +1,13 @@
+package com.ht;
+
+import com.wpn.demo.Demo;
+
+public class C {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demo d = new Demo();
+		d.say();
+	}
+
+}
